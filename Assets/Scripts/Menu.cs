86120534371b5e834cdf7b62c1b39d0f57ac4263
edanurkutay <402554@ogr.ktu.edu.tr>
2022-12-08 09:58:18 +0300

    eda

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StartMenu : MonoBehaviour
+public class Menu : MonoBehaviour
 {
 
     int nextSceneIndex;
