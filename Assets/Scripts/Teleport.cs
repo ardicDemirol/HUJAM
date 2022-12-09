02dd922c71b3ecf5d632805e364145ac8295c6e3
ArdicDemirol <ardc.dmrol@gmail.com>
2022-12-09 22:47:30 +0300

    astronaut package added

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -7,7 +7,7 @@ public class Teleport : MonoBehaviour
 
 
     [SerializeField] Transform[] teleportPoints;
-    private float waitTime = 2f;
+    private float waitTime = 1f;
     private bool teleport = true;
 
     void Start()
@@ -27,7 +27,12 @@ public class Teleport : MonoBehaviour
 
         if (other.tag == "Teleport")
         {
-
+            if (other.gameObject.name == "0" && teleport)
+            {
+                teleport = false;
+                transform.position = teleportPoints[3].transform.position;
+                Invoke(nameof(CanTeleport), waitTime);
+            }
             if (other.gameObject.name == "1" && teleport)
             {
                 teleport = false;
